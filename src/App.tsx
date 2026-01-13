c103d9c9f5f80f04454af4316cc0854a5dfc6e09
DominkoMilic <dmilic04@fesb.hr>
2026-01-13 17:32:06 +0100

    feat: remove test case from app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,6 @@
 import "./App.css";
-import { TotalIncomeCard } from "./components/totalIncomeCard/TotalIncomeCard";
-
 function App() {
-  return (
-    <div>
-      aplikacija
-      <TotalIncomeCard
-        title="Total income 2025"
-        totalIncome={12213.431}
-        subtitle="75 paychecks"
-      />
-    </div>
-  );
+  return <div>aplikacija</div>;
 }
 
 export default App;
