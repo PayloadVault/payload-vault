8e399b10db4277a9d57e9382183ea103e5124d8d
DominkoMilic <dmilic04@fesb.hr>
2026-01-13 18:18:44 +0100

    feat: lock, pdf, folder, user icons added

--- src/components/icons/index.tsx
+++ src/components/icons/index.tsx
@@ -1,1 +1,5 @@
 export { IncomeIcon } from "./IncomeIcon";
+export { PdfIcon } from "./PdfIcon";
+export { FolderIcon } from "./FolderIcon";
+export { UserIcon } from "./UserIcon";
+export { LockIcon } from "./LockIcon";
