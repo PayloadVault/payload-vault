9670d65b3003c205fe4c21668df1f11080d90ef8
DominkoMilic <dmilic04@fesb.hr>
2026-01-13 09:39:47 +0100

    feat: add default index.css styling, implement light and dark theme styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import "./App.css";
 
 function App() {
-  return <div>aplikacija</div>;
+  return (
+    <div className="bg-color-primary text-color-text-main">
+      aplikacija
+      <h1>Hello world</h1>
+      <p>hello world</p>
+    </div>
+  );
 }
 
 export default App;
