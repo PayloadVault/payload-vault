1e7b8538c9aef54ae368fd04373e2a0947692c96
DominkoMilic <dmilic04@fesb.hr>
2026-01-14 17:51:23 +0100

    feat: Refactor icons to use currentColor and add SearchBar component with debounce functionality

--- src/components/icons/SearchIcon.tsx
+++ src/components/icons/SearchIcon.tsx
@@ -10,7 +10,7 @@ export const SearchIcon = ({ size = 24, ...props }: IconProps) => (
   >
     <path
       d="M10 4a6 6 0 1 0 0 12 6 6 0 0 0 0-12zm-8 6a8 8 0 1 1 14.32 4.906l5.387 5.387a1 1 0 0 1-1.414 1.414l-5.387-5.387A8 8 0 0 1 2 10z"
-      fill="#0D0D0D"
+      fill="currentColor"
     />
   </svg>
 );
