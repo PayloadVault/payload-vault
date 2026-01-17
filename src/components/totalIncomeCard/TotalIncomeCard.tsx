a41ef833ebff5f752c0e64add6f3b7cb826da4a0
DominkoMilic <dmilic04@fesb.hr>
2026-01-17 17:25:38 +0100

    feat: add all content cards to home page, add mockdata for page

--- src/components/totalIncomeCard/TotalIncomeCard.tsx
+++ src/components/totalIncomeCard/TotalIncomeCard.tsx
@@ -9,7 +9,7 @@ export const TotalIncomeCard = ({
 }: TotalIncomeCardProps) => {
   return (
     <div className="bg-color-bg-accent/15 rounded-radius-lg flex p-5 justify-center">
-      <div className="w-[95%] flex flex-col items-baseline gap-3">
+      <div className="w-[95%] flex flex-col items-baseline gap-1">
         <h4 className="text-color-text-subtle">{title}</h4>
         <h3 className="text-color-primary">{normalizeProfit(totalIncome)} €</h3>
         {subtitle && <p>{subtitle}</p>}
--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,15 +1,66 @@
+import { useState } from "react";
+import { ContentCard } from "../../components/contentCard/ContentCard";
+import { Dropdown } from "../../components/dropdown/Dropdown";
+import {
+  type DropdownOptions,
+  categorySortOptions,
+} from "../../components/dropdown/DropdownOption";
 import { HeaderHome } from "../../components/header/HeaderHome";
 import { TotalIncomeCard } from "../../components/totalIncomeCard/TotalIncomeCard";
 
 export const HomePage = () => {
+  const [sortSelected, setSortSelected] = useState<
+    DropdownOptions["categorySort"][number]
+  >(categorySortOptions[0]);
+
   return (
     <div className="min-h-screen bg-color-bg">
       <HeaderHome />
-      <main className="mx-auto w-full max-w-5xl px-4 py-6 sm:px-6 lg:px-8">
+      <main className="flex flex-col mx-auto w-full max-w-5xl px-4 py-6 sm:px-6 lg:px-8 gap-10">
         <TotalIncomeCard
           title="Total income"
           subtitle="5 paychecks"
           totalIncome={12300}
+        />
+        <ContentCard
+          variant="allPdf"
+          title="All PDFs"
+          subtitle="Browse all your PDF documents"
+          link="/all-pdfs"
+        />
+        <Dropdown
+          label="Sort Categories"
+          options={categorySortOptions}
+          onSelect={setSortSelected}
+          value={sortSelected}
+        />
+        <ContentCard
+          variant="category"
+          title="Storm & Gas"
+          subtitle="12 documents"
+          profit={5400}
+          link="/category/strom-gas"
+        />
+        <ContentCard
+          variant="category"
+          title="Barmenia Abrechnung"
+          subtitle="12 documents"
+          profit={13400}
+          link="/category/barmenia-abrechnung"
+        />
+        <ContentCard
+          variant="category"
+          title="IKK Abrechnung"
+          subtitle="12 documents"
+          profit={504400}
+          link="/category/ikk-abrechnung"
+        />
+        <ContentCard
+          variant="category"
+          title="Adcuri"
+          subtitle="12 documents"
+          profit={504400}
+          link="/category/adcuri"
         />
       </main>
     </div>
