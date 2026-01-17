308c5e4583c07284b3a16d9a41144147b22e5ee9
DominkoMilic <dmilic04@fesb.hr>
2026-01-17 17:12:43 +0100

    feat: add minor changes to totalIncomeCard, add total income card to home page

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,10 +1,17 @@
 import { HeaderHome } from "../../components/header/HeaderHome";
+import { TotalIncomeCard } from "../../components/totalIncomeCard/TotalIncomeCard";
 
 export const HomePage = () => {
   return (
     <div className="min-h-screen bg-color-bg">
       <HeaderHome />
-      <main className="mx-auto w-full max-w-5xl px-4 py-6 sm:px-6 lg:px-8"></main>
+      <main className="mx-auto w-full max-w-5xl px-4 py-6 sm:px-6 lg:px-8">
+        <TotalIncomeCard
+          title="Total income"
+          subtitle="5 paychecks"
+          totalIncome={12300}
+        />
+      </main>
     </div>
   );
 };
