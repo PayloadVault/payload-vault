dd9a9748709f49886ef5b60a8e0a6c2e67bb75d2
DominkoMilic <dmilic04@fesb.hr>
2026-01-19 20:57:13 +0100

    feat: implement UI for other pages, used mock data for display

--- src/pages/category/CategoryPage.tsx
+++ src/pages/category/CategoryPage.tsx
@@ -1,23 +1,15 @@
-import { Link, useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import { categories } from "../../data/Categories";
 import { useOutletContext } from "react-router-dom";
 import { useLayoutEffect } from "react";
 import { AdcuriPage } from "./AdcuriPage";
+import { OtherPages } from "./OtherPages";
 
 export function CategoryPage() {
   const { slug, subSlug } = useParams();
   const fullSlug = subSlug ? `${slug}/${subSlug}` : slug;
   const category = categories.find((c) => c.slug === fullSlug);
   if (!category) return <h1>Category not found: {fullSlug}</h1>;
-
-  const directChildren = categories.filter((c) => {
-    if (!fullSlug) return false;
-    if (!c.slug.startsWith(fullSlug + "/")) return false;
-    const rest = c.slug.slice((fullSlug + "/").length);
-    return !rest.includes("/");
-  });
-
-  const hasChildren = directChildren.length > 0;
 
   const { setTitle, setSubtitle } = useOutletContext<{
     setTitle: (title: string) => void;
@@ -29,5 +21,13 @@ export function CategoryPage() {
     setSubtitle("year");
   }, [setTitle, setSubtitle, category.title]);
 
-  return <div>{category.title === "Adcuri" && <AdcuriPage />}</div>;
+  return (
+    <div>
+      {category.title === "Adcuri" ? (
+        <AdcuriPage />
+      ) : (
+        <OtherPages title={category.title} />
+      )}
+    </div>
+  );
 }
