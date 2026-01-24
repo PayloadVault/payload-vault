fe3a03ab3a4c34cf35c2c2eafe76234a7b1e7282
dujemilicws <duje.milic@websolutions.hr>
2026-01-24 15:14:53 +0100

    fixed import for deno

--- supabase/functions/extract-data-from-pdf/index.ts
+++ supabase/functions/extract-data-from-pdf/index.ts
@@ -1,6 +1,5 @@
-import { serve } from "https://deno.land/std@0.168.0/http/server.ts";
-import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
-import { GoogleGenerativeAI } from "https://esm.sh/@google/generative-ai";
+import { createClient } from "jsr:@supabase/supabase-js@2";
+import { GoogleGenerativeAI } from "npm:@google/generative-ai";
 import { encode as base64Encode } from "https://deno.land/std@0.168.0/encoding/base64.ts";
 
 type ExtractedAIData = {
@@ -25,7 +24,7 @@ const corsHeaders = {
     "authorization, x-client-info, apikey, content-type",
 };
 
-serve(async (req) => {
+Deno.serve(async (req) => {
   // Handle CORS
   if (req.method === "OPTIONS") {
     return new Response("ok", { headers: corsHeaders });
