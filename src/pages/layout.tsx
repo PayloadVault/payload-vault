3ec255c5330c852f0a98525ad4aa4194a943641f
DominkoMilic <dmilic04@fesb.hr>
2026-01-25 14:06:37 +0100

    feat: Add HeaderHome component to Layout for improved structure

--- src/pages/layout.tsx
+++ src/pages/layout.tsx
@@ -3,6 +3,7 @@ import { Header } from "../components/header/Header";
 import { useState } from "react";
 import { PdfImportFooter } from "../components/pdfImport/PdfImportFooter";
 import { useYear } from "../hooks/year/UseYear";
+import { HeaderHome } from "../components/header/HeaderHome";
 
 export const Layout = () => {
   const [title, setTitle] = useState("");
@@ -10,6 +11,7 @@ export const Layout = () => {
 
   return (
     <div className="flex flex-col min-h-screen overflow-hidden">
+      <HeaderHome />
       <Header title={title} subtitle={year.toString()} />
 
       <main className="flex-1 overflow-y-auto">
