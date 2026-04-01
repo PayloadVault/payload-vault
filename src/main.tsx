dd6614161fa26a0d5a7baacefdb2380aed594089
DominkoMilic <dmilic04@fesb.hr>
2026-04-01 15:19:45 +0200

    feat: integrate vanilla-cookieconsent for cookie management and add theme overrides

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,11 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
+import "./cookieconsent-overrides.css";
 import { ScrollToTop } from "./components/scrollToTop/ScrollToTop";
+import { setupCookieConsent } from "./cookieconsent-config";
+
+setupCookieConsent();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
